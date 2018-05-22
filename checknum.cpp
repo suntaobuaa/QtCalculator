5197846b969b2127985dacdb50df2ac721cc5cc9
suntao <1039495174@qq.com>
2018-05-22 16:36:02 +0800

    fix regexp

--- checknum.cpp
+++ checknum.cpp
@@ -9,7 +9,7 @@ Checknum::Checknum()
 }
 bool Checknum::islegal(QString qs)
 {
-    QRegExp inputrule("^[1-9\(]+.*");
+    QRegExp inputrule("^[0-9\(]+.*[0-9\)]$");
     return inputrule.exactMatch(qs);
 }
 
